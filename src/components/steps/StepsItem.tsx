306fd2c50d4346af76c168d119c68ee790994932
Jérémy Barbet <jeremgraph@gmail.com>
2019-07-21 17:20:08 +0000

    Fix missing component

--- src/components/steps/StepsItem.tsx
+++ src/components/steps/StepsItem.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { injectIntl, Link } from 'gatsby-plugin-intl';
 
 import { Container } from 'components/container/Container';
-import { InlineMarkdown } from 'components/inline-markdown/InlineMarkdown';
 import { AppContext } from 'components/app-layout/AppLayout';
 
+import { Content } from './Content';
 import s from './StepsItem.scss';
 
 interface IProps {
@@ -38,13 +38,10 @@ export const StepsItem = injectIntl(({ count, link, text, media, intl }: IProps)
           to={link}
           onClick={handleClick}
         >
-          <div className={s.stepsItem__content}>
-            <span className={s.stepsItem__count}>{count}</span>
-
-            <span className={s.stepsItem__link}>
-              <InlineMarkdown source={text} />
-            </span>
-          </div>
+          <Content
+            count={count}
+            text={text}
+          />
 
           <div
             className={s.stepsItem__media}
