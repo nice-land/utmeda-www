85e6c84cdde5471e7a96dd424de06ccab2d8696c
Jérémy Barbet <jeremgraph@gmail.com>
2019-07-22 08:36:00 +0000

    Fix scrollwrapper for non-snap behavior

--- src/components/scroll-wrapper/ScrollWrapper.tsx
+++ src/components/scroll-wrapper/ScrollWrapper.tsx
@@ -1,7 +1,7 @@
 import React, { cloneElement, useRef, useEffect, useState, Children } from 'react';
 import { TweenLite, Power3 } from 'gsap';
-import { useKeyDown } from 'hooks/use-keydown';
 
+import { useKeyDown } from 'hooks/use-keydown';
 import 'utils/gsap/ScrollToPlugin';
 
 import s from './ScrollWrapper.scss';
@@ -12,33 +12,11 @@ interface IProps {
   snap?: boolean;
 }
 
-export const ScrollWrapper = ({ children, snap }: IProps) => {
-
+export const ScrollWrapper = ({ children, className, snap }: IProps) => {
   const container = useRef<HTMLOListElement>(null);
   const [activeSlide, setActiveSlide] = useState(0);
   const keys = useKeyDown();
-
   const slidesCount = Children.count(children);
-
-  useEffect(() => {
-    // on arrow right press
-    if (keys.includes(39)) {
-      handleNext();
-    }
-    // on arrow left press
-    if (keys.includes(37)) {
-      handlePrev();
-    }
-    // on space
-    if (keys.includes(32)) {
-      handleNext();
-    }
-  }, [keys]);
-
-  useEffect(() => {
-    animate();
-
-  }, [activeSlide]);
 
   const handleNext = () => {
     const index =
@@ -71,15 +49,46 @@ export const ScrollWrapper = ({ children, snap }: IProps) => {
     container.current!.scrollLeft = (width / slidesCount) * activeSlide;
   };
 
+  useEffect(() => {
+    if (!snap) {
+      return;
+    }
+
+    // on arrow right press
+    if (keys.includes(39)) {
+      handleNext();
+    }
+    // on arrow left press
+    if (keys.includes(37)) {
+      handlePrev();
+    }
+    // on space
+    if (keys.includes(32)) {
+      handleNext();
+    }
+  }, [keys]);
+
+  useEffect(() => {
+    if (!snap) {
+      return;
+    }
+
+    animate();
+  }, [activeSlide, snap]);
+
   return (
-    <div className={s('scrollWrapper', { snap })}>
+    <div className={s(s.scrollWrapper, snap)}>
       <ol className={s.scrollWrapper__inner} ref={container}>
         {Children.map(children, (child) => (
-          <li className={s.scrollWrapper__item}>
+          <li className={s(s.scrollWrapper__item, className)}>
             {cloneElement(child as React.ReactElement<any>, { setActive: setActiveSlide })}
           </li>
         ))}
       </ol>
     </div>
   );
+};
+
+ScrollWrapper.defaultProps = {
+  snap: true,
 };
