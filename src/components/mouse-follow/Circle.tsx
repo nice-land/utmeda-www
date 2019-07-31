28f9c6bff786e203b355f817d087d44f996a1bdc
Jérémy Barbet <jeremgraph@gmail.com>
2019-07-31 13:53:35 +0000

    Bigger text

--- src/components/mouse-follow/Circle.tsx
+++ src/components/mouse-follow/Circle.tsx
@@ -7,7 +7,7 @@ interface ICircleProps {
 }
 
 export const Circle = ({ text }: ICircleProps) => {
-  const length = String(text).length > 10 ? 2 : 3;
+  const length = String(text).length > 10 ? 2 : 4;
   const items = Array.isArray(text) ? text : Array.from({ length }).map(() => text);
 
   return (
