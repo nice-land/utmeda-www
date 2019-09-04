4a925e688926c57572088b5906f4f4825728962c
Davíð Arnarsson <davidarnarsson@gmail.com>
2019-09-04 22:30:42 +0000

    new attempt at a webgl carousel

--- src/components/steps/StepsItem.tsx
+++ src/components/steps/StepsItem.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { injectIntl, Link } from 'gatsby-plugin-intl';
-import { useInView } from 'react-intersection-observer';
-
 import { Container } from 'components/container/Container';
 import { AppContext } from 'components/app-layout/AppLayout';
 
@@ -15,40 +13,20 @@ interface IProps {
   text: string;
   media: string;
   index: number;
-  setActive: (args: number) => void;
+  
 }
 
-export const StepsItem = injectIntl(({ count, link, text, media, intl, setActive, index }: IProps) => {
-  const { mouseEnter, mouseLeave } = React.useContext(AppContext);
-  const threshold = 0.5;
-  const [ref, active] = useInView({ threshold });
-
-  const handleMouseEnter = () => {
-    mouseEnter({
-      text: intl.formatMessage({ id: 'step_watch' }),
-      icon: 'play',
-    });
-  };
-
-  const handleMouseLeave = () => {
-    mouseLeave();
-  };
+export const StepsItem = injectIntl(({ count, link, text }: IProps) => {
+  const { mouseLeave } = React.useContext(AppContext);
+  
 
   const handleClick = () => {
     mouseLeave();
   };
 
-  useEffect(() => {
-
-    if (active) {
-      setActive(index);
-      // console.log(active, index);
-    }
-
-  }, [active]);
 
   return (
-    <div className={s.stepsItem} ref={ref}>
+    <div className={s.stepsItem}>
       <Container>
         <Link
           className={s.stepsItem__wrapper}
@@ -59,14 +37,6 @@ export const StepsItem = injectIntl(({ count, link, text, media, intl, setActive
             count={count}
             text={text}
           />
-
-          <div
-            className={s.stepsItem__media}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <img src={media} alt="" />
-          </div>
         </Link>
       </Container>
     </div>
