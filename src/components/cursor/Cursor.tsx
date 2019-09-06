3abb7536547df03e268283b520876546a3878633
Davíð Arnarsson <davidarnarsson@gmail.com>
2019-09-06 21:33:58 +0000

    fix cursor in build

--- src/components/cursor/Cursor.tsx
+++ src/components/cursor/Cursor.tsx
@@ -1,19 +1,26 @@
-import React, { useContext, useEffect } from 'react';
-import { useSpring, animated as a } from 'react-spring';
-import { useMove } from 'react-use-gesture';
+import React, { useContext, useEffect } from "react";
+import { useSpring, animated as a } from "react-spring";
+import { useMove } from "react-use-gesture";
 
-import Play from 'assets/svg/play.svg';
-import Mouse from 'assets/svg/mouse.svg';
+import Play from "assets/svg/play.svg";
+import Mouse from "assets/svg/mouse.svg";
 
-import { AppContext } from 'components/app-layout/AppLayout';
+import { AppContext } from "components/app-layout/AppLayout";
 
-import { Circle } from './Circle';
-import s from './Cursor.scss';
+import { Circle } from "./Circle";
+import s from "./Cursor.scss";
 
 export const Cursor = () => {
+  if (typeof window === "undefined") {
+    return null;
+  }
+
   const { cursorText, cursorIcon, isMediaHovered } = useContext(AppContext);
-  const [{ xy, hovering }, set] = useSpring(() => ({ xy: [0, 0], hovering: 0 }));
-  const bind = useMove((d) => set({ xy: d.xy }), { domTarget: window });
+  const [{ xy, hovering }, set] = useSpring(() => ({
+    xy: [0, 0],
+    hovering: 0
+  }));
+  const bind = useMove(d => set({ xy: d.xy }), { domTarget: window });
 
   useEffect(bind as any, [bind]);
 
@@ -23,33 +30,38 @@ export const Cursor = () => {
 
   const opacity = hovering.interpolate({ range: [0, 1], output: [1, 0] });
 
+  const scale = (output: number[]) =>
+    hovering
+      .interpolate({ range: [0, 1], output })
+      .interpolate(r => `scale(${r})`);
+
   return (
     <a.div
       className={s.cursor}
-      style={{ transform: xy.interpolate((x: number, y: number) => `translate3D(${x}px, ${y}px, 0)`) }}
+      style={{
+        transform: xy.interpolate(
+          (x: number, y: number) => `translate3D(${x}px, ${y}px, 0)`
+        )
+      }}
     >
       <a.div
         className={s.cursor__inner}
         style={{
-          opacity,
+          opacity
         }}
       />
       <a.div
         className={s.cursor__stroke}
         style={{
           opacity,
-          transform: hovering
-            .interpolate({ range: [0, 1], output: [1, 1.3] })
-            .interpolate((r) => `scale(${r})`),
+          transform: scale([1, 1.3])
         }}
       />
 
       <a.div
         style={{
           opacity: hovering,
-          transform: hovering
-            .interpolate({ range: [0, 1], output: [1.2, 1] })
-            .interpolate((r) => `scale(${r})`),
+          transform: scale([1.2, 1])
         }}
       >
         <Circle text={cursorText} />
@@ -59,13 +71,11 @@ export const Cursor = () => {
         className={s.cursor__icon}
         style={{
           opacity: hovering,
-          transform: hovering
-            .interpolate({ range: [0, 1], output: [0.8, 1] })
-            .interpolate((r) => `scale(${r})`),
+          transform: scale([0.8, 1])
         }}
       >
-        {cursorIcon === 'play' && <Play />}
-        {cursorIcon === 'mouse' && <Mouse />}
+        {cursorIcon === "play" && <Play />}
+        {cursorIcon === "mouse" && <Mouse />}
       </a.div>
     </a.div>
   );
