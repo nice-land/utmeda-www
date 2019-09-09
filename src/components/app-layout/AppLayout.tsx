d7843d001eee564da9b91140ff69a9efa108c5aa
Davíð Arnarsson <davidarnarsson@gmail.com>
2019-09-09 11:17:36 +0000

    disable context menu

--- src/components/app-layout/AppLayout.tsx
+++ src/components/app-layout/AppLayout.tsx
@@ -1,41 +1,33 @@
-import * as React from "react";
-import { Link, FormattedMessage } from "gatsby-plugin-intl";
+import * as React from 'react';
+import { Link, FormattedMessage } from 'gatsby-plugin-intl';
 
-import { Header } from "components/header/Header";
-import { Footer } from "components/footer/Footer";
-import { Language } from "components/language/Language";
-import { Devtools } from "components/devtools/Devtools";
-import { PhoneBubble } from "components/phone-bubble/PhoneBubble";
-import { Cursor } from "components/cursor/Cursor";
-import { OrientationDetector } from "components/orientation-detector/OrientationDetector";
-import { useMobile } from "hooks/use-mobile";
+import { Header } from 'components/header/Header';
+import { Footer } from 'components/footer/Footer';
+import { Language } from 'components/language/Language';
+import { Devtools } from 'components/devtools/Devtools';
+import { PhoneBubble } from 'components/phone-bubble/PhoneBubble';
+import { Cursor } from 'components/cursor/Cursor';
+import { OrientationDetector } from 'components/orientation-detector/OrientationDetector';
+import { useMobile } from 'hooks/use-mobile';
 
-import s from "./AppLayout.scss";
+import s from './AppLayout.scss';
 
 interface IAppLayoutProps {
   children: React.ReactNode;
 }
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 
 export const AppContext = React.createContext({}) as any;
 
 export default ({ children }: IAppLayoutProps) => {
-  const [cursorText, setCursorText] = React.useState<
-    undefined | string | string[]
-  >();
+  const [cursorText, setCursorText] = React.useState<undefined | string | string[]>();
   const [cursorIcon, setCursorIcon] = React.useState<undefined | string>();
   const [isMediaHovered, setMediaHovered] = React.useState(false);
   const [activeStep, setActiveStep] = React.useState<number | null>(null);
   const timer = React.useRef<any>(null);
 
-  const mouseEnter = ({
-    text,
-    icon
-  }: {
-    text: string | undefined;
-    icon: "play" | "mouse";
-  }) => {
+  const mouseEnter = ({ text, icon }: { text: string | undefined; icon: 'play' | 'mouse' }) => {
     clearTimeout(timer.current);
 
     if (text) {
@@ -55,12 +47,17 @@ export default ({ children }: IAppLayoutProps) => {
     }, 400);
   };
 
-  const handleClick = e => {
+  const handleClick = (e) => {
     setActiveStep(null);
   };
 
+  const onContextMenu = (e: React.MouseEvent) => {
+    e.preventDefault();
+    return false;
+  };
+
   return (
-    <div className={s.layout}>
+    <div className={s.layout} onContextMenuCapture={onContextMenu}>
       <OrientationDetector />
 
       <AppContext.Provider
@@ -72,7 +69,7 @@ export default ({ children }: IAppLayoutProps) => {
           mouseEnter,
           mouseLeave,
           activeStep,
-          setActiveStep
+          setActiveStep,
         }}
       >
         <Header>
@@ -80,10 +77,7 @@ export default ({ children }: IAppLayoutProps) => {
             <FormattedMessage id="navigation.steps" defaultMessage="Skrefin" />
           </Link>
           <Link to="/about">
-            <FormattedMessage
-              id="navigation.about"
-              defaultMessage="Um átakið"
-            />
+            <FormattedMessage id="navigation.about" defaultMessage="Um átakið" />
           </Link>
           <Language />
         </Header>
