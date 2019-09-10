2c06e95d8c29e8403fc148e833abb0939beb5137
Davíð Arnarsson <davidarnarsson@gmail.com>
2019-09-10 08:43:12 +0000

    tighten cursor spring, its toiter now

--- src/components/cursor/Cursor.tsx
+++ src/components/cursor/Cursor.tsx
@@ -1,15 +1,15 @@
-import React, { useContext, useEffect } from "react";
-import { useSpring, animated as a } from "react-spring";
-import { useMove } from "react-use-gesture";
+import React, { useContext, useEffect } from 'react';
+import { useSpring, animated as a, config } from 'react-spring';
+import { useMove } from 'react-use-gesture';
 
-import Play from "assets/svg/play.svg";
-import Mouse from "assets/svg/mouse.svg";
+import Play from 'assets/svg/play.svg';
+import Mouse from 'assets/svg/mouse.svg';
 
-import { AppContext } from "components/app-layout/AppLayout";
-import { useMobile } from "hooks/use-mobile";
+import { AppContext } from 'components/app-layout/AppLayout';
+import { useMobile } from 'hooks/use-mobile';
 
-import { Circle } from "./Circle";
-import s from "./Cursor.scss";
+import { Circle } from './Circle';
+import s from './Cursor.scss';
 
 export const Cursor = () => {
   const isMobile = useMobile();
@@ -17,10 +17,15 @@ export const Cursor = () => {
   const { cursorText, cursorIcon, isMediaHovered } = useContext(AppContext);
   const [{ xy, hovering }, set] = useSpring(() => ({
     xy: [0, 0],
-    hovering: 0
+    hovering: 0,
+    config: {
+      tension: 210,
+      friction: 20,
+      mass: 0.5,
+    },
   }));
-  const bind = useMove(d => set({ xy: d.xy }), {
-    domTarget: typeof window === "undefined" ? null : window
+  const bind = useMove((d) => set({ xy: d.xy }), {
+    domTarget: typeof window === 'undefined' ? null : window,
   });
 
   useEffect(bind as any, [bind]);
@@ -32,37 +37,33 @@ export const Cursor = () => {
   const opacity = hovering.interpolate({ range: [0, 1], output: [1, 0] });
 
   const scale = (output: number[]) =>
-    hovering
-      .interpolate({ range: [0, 1], output })
-      .interpolate(r => `scale(${r})`);
+    hovering.interpolate({ range: [0, 1], output }).interpolate((r) => `scale(${r})`);
 
   return (
     <a.div
       className={s(s.cursor, { isMobile })}
       style={{
-        transform: xy.interpolate(
-          (x: number, y: number) => `translate3D(${x}px, ${y}px, 0)`
-        )
+        transform: xy.interpolate((x: number, y: number) => `translate3D(${x}px, ${y}px, 0)`),
       }}
     >
       <a.div
         className={s.cursor__inner}
         style={{
-          opacity
+          opacity,
         }}
       />
       <a.div
         className={s.cursor__stroke}
         style={{
           opacity,
-          transform: scale([1, 1.3])
+          transform: scale([1, 1.3]),
         }}
       />
 
       <a.div
         style={{
           opacity: hovering,
-          transform: scale([1.2, 1])
+          transform: scale([1.2, 1]),
         }}
       >
         <Circle text={cursorText} />
@@ -72,11 +73,11 @@ export const Cursor = () => {
         className={s.cursor__icon}
         style={{
           opacity: hovering,
-          transform: scale([0.8, 1])
+          transform: scale([0.8, 1]),
         }}
       >
-        {cursorIcon === "play" && <Play />}
-        {cursorIcon === "mouse" && <Mouse />}
+        {cursorIcon === 'play' && <Play />}
+        {cursorIcon === 'mouse' && <Mouse />}
       </a.div>
     </a.div>
   );
