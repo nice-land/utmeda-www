5fb561d316f9fde640499bdbd6ea014d2c5ac7ba
Jérémy Barbet <jeremgraph@gmail.com>
2019-09-12 14:09:31 +0000

    Type canvas back

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,4 +1,6 @@
-export let Canvas = () => null;
+import { CanvasProps } from 'react-three-fiber';
+
+export let Canvas = (_props: CanvasProps) => null;
 
 if (typeof window !== 'undefined') {
   // tslint:disable-next-line: no-var-requires
